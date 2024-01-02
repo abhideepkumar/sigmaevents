74698c8dd27ded02f3c34d022854cc50bdea964f
Abhideep Kumar <itsabhideep@gmail.com>
2024-01-02 20:36:07 +0530

    some fixes in login component

--- components/login.js
+++ components/login.js
@@ -11,47 +11,51 @@ const Login = () => {
 
   useEffect(() => {
     // Fetch user data and set cookies if the user is logged in
-    fetchUserData();
+    if (status === "authenticated") {
+      fetchUserData();
+    }
   }, [status]);
 
-  const fetchUserData = () => {
-    fetch("/api/auth/checkuser")
-      .then((response) => response.json())
-      .then((data) => {
-        // Redirect to settings if user data is not available
-        if (data == null) {
-          router.push("/setting");
-        } else {
-          // Set cookies for user data
-          Object.keys(data).forEach((key) => {
-            Cookies.set(key, data[key], { expires: 30 });
-          });
-        }
-      })
-      .catch((error) => {
-        console.error("Error while fetching user data from Login:", error);
-      });
+  const fetchUserData = async () => {
+    try {
+      const response = await fetch("/api/auth/checkuser");
+      if (!response.ok) {
+        throw new Error("Failed to fetch user data");
+      }
+      const data = await response.json();
+
+      if (data === null) {
+        router.push("/setting");
+      } else {
+        // Set cookies for user data
+        Object.keys(data).forEach((key) => {
+          Cookies.set(key, data[key], { expires: 30 });
+        });
+      }
+    } catch (error) {
+      console.error("Error while fetching user data from Login:", error);
+    }
   };
 
   return (
     <div className="flex items-center">
       {session ? (
         // Display settings link if the user is logged in
-        <Link href="/setting" className="">
+        <Link href="/setting">
           <Image
-            href="/"
             src="/profile.svg"
             width={0}
             height={0}
             alt="settings"
             className="w-6"
+            priority
           />
         </Link>
       ) : (
         // Display login button if the user is not logged in
         <button
           onClick={() => {
-            signIn("google", process.env.NEXTAUTH_URL);
+            signIn("google", { callbackUrl: process.env.NEXTAUTH_URL });
           }}
           className="font-extrabold text-xs lg:text-sm border border-black px-4 py-1 lg:px-8 lg:py-2 hover:bg-black hover:text-white transition-colors duration-200"
         >
