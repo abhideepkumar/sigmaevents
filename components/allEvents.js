2571cbc417504d2487635d90784f268b2ce3014d
JrG-One <ojaswavarshney27@gmail.com>
2024-01-03 11:03:36 +0530

    feedback Button added

--- components/allEvents.js
+++ components/allEvents.js
@@ -8,6 +8,10 @@ const AllEvents = () => {
   const [loading, setLoading] = useState(true);
   const { data: session } = useSession();
   const userId = Cookies.get("_id");
+  const [selectedEvent, setSelectedEvent] = useState(null);
+  const [showFeedbackForm, setShowFeedbackForm] = useState(false);
+  const [rating, setRating] = useState("");
+  const [detailedFeedback, setDetailedFeedback] = useState("");
   let Allset = userId !== undefined;
 
   useEffect(() => {
@@ -46,7 +50,9 @@ const AllEvents = () => {
   // Function to handle event registration
   const handleRegister = async (event) => {
     try {
-      const response = await fetch(`/api/auth/register?eventId=${event._id}&userId=${userId}`);
+      const response = await fetch(
+        `/api/auth/register?eventId=${event._id}&userId=${userId}`
+      );
       if (response.ok) {
         window.location.reload();
       } else {
@@ -58,12 +64,46 @@ const AllEvents = () => {
     }
   };
 
+  // Function to handle feedback submission
+  const handleFeedback = async () => {
+    try {
+      // Ensure both rating and detailed feedback are provided
+      if (!rating || !detailedFeedback) {
+        console.log("Please provide both rating and detailed feedback.");
+        return;
+      }
+
+      // Feedback data to be submitted
+      const feedbackData = {
+        eventId: selectedEvent._id,
+        userId: userId,
+        rating: rating,
+        detailedFeedback: detailedFeedback,
+      };
+
+      // Send POST request to submit feedback
+      // You need to implement the server-side logic for this part
+      console.log("Feedback data:", feedbackData);
+
+      // Close the feedback form
+      setShowFeedbackForm(false);
+
+      // Reset the feedback form state
+      setRating("");
+      setDetailedFeedback("");
+    } catch (error) {
+      console.error("Error while submitting feedback:", error);
+    }
+  };
+
   return (
     <main className="container p-4 mx-auto">
       <h1 className="text-3xl font-bold mb-6">Events</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {loading ? (
-          <h1 className="text-2xl font-semibold text-gray-600">Loading... Please Wait</h1>
+          <h1 className="text-2xl font-semibold text-gray-600">
+            Loading... Please Wait
+          </h1>
         ) : (
           events.map((event) => (
             <div
@@ -74,7 +114,9 @@ const AllEvents = () => {
                 {/* Display event image */}
                 <Image
                   src={`https://source.unsplash.com/480x360/?code?${event._id}`}
-                  loader={() => `https://source.unsplash.com/480x360/?code?${event._id}`}
+                  loader={() =>
+                    `https://source.unsplash.com/480x360/?code?${event._id}`
+                  }
                   alt="Event Image"
                   width={480}
                   height={360}
@@ -83,11 +125,16 @@ const AllEvents = () => {
               </div>
               {/* Display event details */}
               <div>
-                <h2 className="mt-1 text-xl font-semibold">{event.title || "Not Mentioned"}</h2>
+                <h2 className="mt-1 text-xl font-semibold">
+                  {event.title || "Not Mentioned"}
+                </h2>
                 <p className="mt-1">{event.desc || "Not Mentioned"}</p>
-                <p className="mt-1 text-gray-600">on {event.date || "Not Mentioned"}</p>
+                <p className="mt-1 text-gray-600">
+                  on {event.date || "Not Mentioned"}
+                </p>
                 <p className="mt-1">
-                  <strong>Location:</strong> {event.location?.type || "Not Mentioned"}
+                  <strong>Location:</strong>{" "}
+                  {event.location?.type || "Not Mentioned"}
                 </p>
                 <p className="mt-1">
                   <strong>Deadline:</strong> {event.deadline || "Not Mentioned"}
@@ -109,7 +156,13 @@ const AllEvents = () => {
                         : "bg-black text-white shadow-md hover:shadow-2xl"
                     }`}
                     onClick={() => {
-                      if (confirm('Do you want to confirm Registration for the event "' + event.title + `"`)) {
+                      if (
+                        confirm(
+                          'Do you want to confirm Registration for the event "' +
+                            event.title +
+                            `"`
+                        )
+                      ) {
                         handleRegister(event);
                       } else {
                         console.log("Registration cancelled");
@@ -117,18 +170,75 @@ const AllEvents = () => {
                     }}
                     disabled={checkRegister(event.registered)}
                   >
-                    {checkRegister(event.registered) ? "Registered" : "Register"}
+                    {checkRegister(event.registered)
+                      ? "Registered"
+                      : "Register"}
+                  </button>
+                )}
+                {/* Feedback button */}
+                {session && event.feedback && (
+                  <button
+                    className="ml-4 px-6 py-2 rounded-3xl hover:shadow-2xl bg-blue-500 text-white"
+                    onClick={() => {
+                      setSelectedEvent(event);
+                      setShowFeedbackForm(true);
+                    }}
+                  >
+                    Feedback
                   </button>
                 )}
               </div>
               {/* Display message for users to login and fill data in settings */}
               {!Allset && !session && (
-                <div className="text-red-600">Login and fill data in settings to Register</div>
+                <div className="text-red-600">
+                  Login and fill data in settings to Register
+                </div>
               )}
             </div>
           ))
         )}
       </div>
+
+      {/* Feedback Form */}
+      {showFeedbackForm && (
+        <div className="fixed inset-0 bg-gray-700 bg-opacity-75 flex justify-center items-center">
+          <div className="bg-white p-6 rounded-md">
+            <h2 className="text-2xl font-bold mb-4">
+              Feedback Form {/*for {event.title}*/}
+            </h2>
+            <div className="mb-4">
+              <label className="block text-sm font-medium text-gray-700">
+                Rating:
+              </label>
+              <input
+                type="number"
+                min="1"
+                max="5"
+                placeholder="Enter rating (1-5)"
+                className="mt-1 p-2 border rounded-md w-full"
+                value={rating}
+                onChange={(e) => setRating(e.target.value)}
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-sm font-medium text-gray-700">
+                Detailed Feedback:
+              </label>
+              <textarea
+                className="mt-1 p-2 border rounded-md w-full"
+                value={detailedFeedback}
+                onChange={(e) => setDetailedFeedback(e.target.value)}
+              ></textarea>
+            </div>
+            <button
+              className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+              onClick={handleFeedback}
+            >
+              Submit Feedback
+            </button>
+          </div>
+        </div>
+      )}
     </main>
   );
 };
