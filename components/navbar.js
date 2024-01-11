90ef15bc88f33c59d7c6fccc1c1c9a97aac6b553
Abhideep Kumar <itsabhideep@gmail.com>
2024-01-12 01:52:00 +0530

    improved navbar

--- components/navbar.js
+++ components/navbar.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import Login from "./login";
 import Image from "next/image";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
   return (
     <>
       <div className="lg:hidden flex justify-between items-center p-6 theme">
@@ -18,29 +28,23 @@ const Navbar = () => {
         <div className="flex gap-6 items-center">
           <Login />
           <Image
-            src="/icon-hamburger.svg"
+            src={menuOpen ? "/icon-close.svg" : "/icon-hamburger.svg"}
             width={0}
             height={0}
             alt="menu"
             className="mix-blend-multiply w-6 z-50 menu-icon"
-            onClick={() => {
-              document.querySelector(".menu").classList.toggle("hidden");
-              if (
-                document
-                  .querySelector(".menu-icon")
-                  .src.indexOf("icon-hamburger.svg") !== -1
-              )
-                document.querySelector(".menu-icon").src = "icon-close.svg";
-              else
-                document.querySelector(".menu-icon").src = "icon-hamburger.svg";
-            }}
+            onClick={toggleMenu}
           />
-          <div className="hidden absolute top-0 right-0 w-full h-full z-40 bg-white menu">
+          <div
+            className={`${
+              menuOpen ? "block" : "hidden"
+            } absolute top-0 right-0 w-full h-full z-40 bg-white menu`}
+          >
             <div className="flex flex-col items-center gap-12 mt-32">
-              <Link href="/" className="font-extrabold text-sm">
+              <Link href="/" className="font-extrabold text-sm" onClick={closeMenu}>
                 HOME
               </Link>
-              <Link href="/events" className="font-extrabold text-sm">
+              <Link href="/events" className="font-extrabold text-sm" onClick={closeMenu}>
                 EVENTS
               </Link>
             </div>
